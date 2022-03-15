164b1e58faf0ece37b2c725decbd783e89076ee2
kekzdealer <kekzdealer@gmail.com>
2022-03-15 23:41:29 +0100

    J2EE update

--- src/main/java/at/kurumi/BotStart.java
+++ src/main/java/at/kurumi/BotStart.java
@@ -1,18 +1,19 @@
 package at.kurumi;
 
 import at.kurumi.calendar.CalendarProgram;
-import at.kurumi.calendar.Clock;
-import at.kurumi.calendar.EventSLO;
 import at.kurumi.commands.Command;
 import at.kurumi.register.RegisterProgram;
 import at.kurumi.user.UserProgram;
-import at.kurumi.user.UserSLO;
 import at.kurumi.work.WorkProgram;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.interaction.ApplicationCommandInteractionEvent;
 import discord4j.core.event.domain.interaction.ChatInputAutoCompleteEvent;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import jakarta.ejb.Startup;
+import jakarta.inject.Inject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import reactor.core.publisher.Mono;
@@ -21,6 +22,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+@Startup
 public class BotStart {
 
     private static final Logger LOG = LogManager.getLogger();
@@ -28,42 +30,47 @@ public class BotStart {
 
     private final Map<String, Command> commands = new HashMap<>();
 
-    public GatewayDiscordClient login(String token) {
-        final var gatewayDiscordClient = DiscordClientBuilder.create(token).build()
+    /*
+    This list might grow very long in the future, so I'm using field injection instead of constructor injection.
+    I don't want a constructor with more than or so parameters.
+     */
+    @Inject private CalendarProgram calendarProgram;
+    @Inject private UserProgram userProgram;
+    @Inject private ShutdownProgram shutdownProgram;
+    @Inject private RegisterProgram registerProgram;
+    @Inject private WorkProgram workProgram;
+
+    private GatewayDiscordClient discordClient;
+
+    @PostConstruct
+    public void startup() {
+        discordClient = DiscordClientBuilder.create("token").build()
                 .login()
                 .doOnError(e -> LOG.error("Failed to authenticate with Discord"))
                 .doOnSuccess(e -> LOG.info("Connected to Discord"))
                 .block();
-
-        if (gatewayDiscordClient == null) {
+        if (discordClient == null) {
             throw new RuntimeException("GatewayDiscordClient is null");
         }
 
-        gatewayDiscordClient.on(ReadyEvent.class)
+        discordClient.on(ReadyEvent.class)
                 .doOnNext(e -> LOG.info("Logged in as {}", e.getSelf().getUsername()))
                 .subscribe();
+        discordClient.on(ApplicationCommandInteractionEvent.class, this::delegateToProgram)
+                .subscribe();
+        discordClient.on(ChatInputAutoCompleteEvent.class, this::delegateAutoComplete)
+                .subscribe();
 
-        return gatewayDiscordClient;
+        registerCommand(calendarProgram );
+        registerCommand(userProgram);
+        registerCommand(shutdownProgram);
+        registerCommand(registerProgram);
+        registerCommand(workProgram);
     }
 
-    public void setup(GatewayDiscordClient client) {
-        final var database = new Database();
-        final var userSLO = new UserSLO(database);
-        final var eventSLO = new EventSLO(database);
-
-        final var clock = new Clock();
-
-        registerCommand(client, new CalendarProgram(eventSLO, userSLO));
-        registerCommand(client, new UserProgram(userSLO));
-        registerCommand(client, new ShutdownProgram());
-        registerCommand(client, new RegisterProgram(userSLO));
-        registerCommand(client, new WorkProgram(clock));
-
-        client.on(ApplicationCommandInteractionEvent.class, this::delegateToProgram)
-                .subscribe();
-        client.on(ChatInputAutoCompleteEvent.class, this::delegateAutoComplete)
-                .subscribe();
-        client.onDisconnect().block();
+    @PreDestroy
+    public void shutdown() {
+        discordClient.logout();
     }
 
     private Mono<Void> delegateToProgram(ApplicationCommandInteractionEvent event) {
@@ -78,19 +85,14 @@ public class BotStart {
                 .orElseThrow(IllegalStateException::new);
     }
 
-    private void registerCommand(GatewayDiscordClient client, Command command) {
-        Command.guildCommand(client,
+    private void registerCommand(Command command) {
+        Command.guildCommand(discordClient,
                 KURIS_LAB_GUILD_ID,
                 command.getName(),
                 command.getDescription(),
                 command.getOptions());
         commands.put(command.getName(), command);
         LOG.info("Registered {} command", command.getName());
-    }
-
-    public static void main(String[] args) {
-        final var o = new BotStart();
-        o.setup(o.login(args[0]));
     }
 
 }
