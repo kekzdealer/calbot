a9e9f70c29d4b1e2be0181108845a478c1519cd3
kekzdealer <kekzdealer@gmail.com>
2022-06-28 13:39:36 +0200

    1339h

--- src/main/java/at/kurumi/HealthCheckResponder.java
+++ src/main/java/at/kurumi/HealthCheckResponder.java
@@ -2,13 +2,16 @@ package at.kurumi;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/healthCheck")
 public class HealthCheckResponder {
 
     @GET
-    public Response healthCheck() {
-        return Response.ok().build();
+    @Produces(MediaType.TEXT_PLAIN)
+    public String healthCheck() {
+        return "healthy";
     }
 }
