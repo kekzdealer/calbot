6fe6a734c9bd8087a17640b17d01f1a10e844919
kekzdealer <kekzdealer@gmail.com>
2022-06-28 21:36:42 +0200

    maybe?

--- src/main/java/at/kurumi/HeartBeat.java
+++ src/main/java/at/kurumi/HeartBeat.java
@@ -7,7 +7,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/healthCheck")
+@Path("/heartbeat")
 public class HeartBeat extends HttpServlet {
 
     @GET
