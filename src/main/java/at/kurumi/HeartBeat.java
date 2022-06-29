9bd824e70cda6e33ab9e2e0a2eee72fb85bd70c5
kekzdealer <kekzdealer@gmail.com>
2022-06-29 21:24:49 +0200

    fkn work logging

--- src/main/java/at/kurumi/HeartBeat.java
+++ src/main/java/at/kurumi/HeartBeat.java
@@ -3,6 +3,7 @@ package at.kurumi;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import java.util.logging.Logger;
 
 /**
  * HTTP Endpoint to check if payara is still running.
@@ -10,8 +11,11 @@ import javax.ws.rs.core.Response;
 @Path("/heartbeat")
 public class HeartBeat {
 
+    private static final Logger LOG = Logger.getLogger("HeartBeat");
+
     @GET
     public Response healthCheck() {
+        LOG.severe("Beat");
         return Response.ok().entity("I'm alive\n").build();
     }
 }
