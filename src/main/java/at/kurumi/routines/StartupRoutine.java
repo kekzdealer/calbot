04e543c3e01c0e58830f7ec41354c33bc565b912
kekzdealer <kekzdealer@gmail.com>
2022-06-30 09:49:33 +0200

    tf is going on

--- src/main/java/at/kurumi/routines/StartupRoutine.java
+++ src/main/java/at/kurumi/routines/StartupRoutine.java
@@ -3,11 +3,12 @@ package at.kurumi.routines;
 import at.kurumi.docker.ContainerAvailableEvent;
 import at.kurumi.docker.DockerInterface;
 import jakarta.annotation.PostConstruct;
-import jakarta.ejb.Singleton;
+
 import jakarta.ejb.Startup;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
 
+import javax.inject.Singleton;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
