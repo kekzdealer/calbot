b06ab32cfa9c95d7fd1da34480b04e500078a683
kekzdealer <kekzdealer@gmail.com>
2022-06-30 09:56:46 +0200

    tf is going on

--- src/main/java/at/kurumi/routines/StartupRoutine.java
+++ src/main/java/at/kurumi/routines/StartupRoutine.java
@@ -4,27 +4,26 @@ import at.kurumi.docker.ContainerAvailableEvent;
 import at.kurumi.docker.DockerInterface;
 import jakarta.annotation.PostConstruct;
 
-import jakarta.ejb.Startup;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.Initialized;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
 
-import javax.inject.Singleton;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-@Singleton
-@Startup
+@ApplicationScoped
 public class StartupRoutine {
 
-    private final DockerInterface dockerInterface;
-
-    @Inject
-    public StartupRoutine(DockerInterface dockerInterface) {
-        this.dockerInterface = dockerInterface;
-    }
+//    private final DockerInterface dockerInterface;
+//
+//    @Inject
+//    public StartupRoutine(DockerInterface dockerInterface) {
+//        this.dockerInterface = dockerInterface;
+//    }
 
     @PostConstruct
-    public void onStart() {
+    public void onStart(@Observes @Initialized(ApplicationScoped.class) Object pointless) {
         System.out.println("ALLLLLLLLLLLLLLLOOOOOOOOOOOOOOOOOOO");
         final var rootLogger = Logger.getLogger("");
         final var logLevel = Level.ALL;
@@ -34,7 +33,7 @@ public class StartupRoutine {
         }
         rootLogger.severe("Set log level to: " + logLevel.getName());
 
-        dockerInterface.newContainer("/at/kurumi/routines/kurumi-db.dc", true);
+        //dockerInterface.newContainer("/at/kurumi/routines/kurumi-db.dc", true);
     }
 
     private void startDiscordInterface(@Observes ContainerAvailableEvent condition) {
