21f7a95cf91655dbfe021b3dcbab159228dcebba
kekzdealer <kekzdealer@gmail.com>
2022-06-30 10:04:11 +0200

    tf is going on

--- src/main/java/at/kurumi/HeartBeat.java
+++ src/main/java/at/kurumi/HeartBeat.java
@@ -1,5 +1,8 @@
 package at.kurumi;
 
+import at.kurumi.routines.StartupRoutine;
+import jakarta.inject.Inject;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
@@ -10,8 +13,12 @@ import javax.ws.rs.core.Response;
 @Path("/heartbeat")
 public class HeartBeat {
 
+    @Inject
+    private StartupRoutine fuck;
+
     @GET
     public Response healthCheck() {
+        fuck.toString();
         return Response.ok().entity("I'm alive\n").build();
     }
 }
