a8e2b2890353c8749f904ee4152e7912f8a2cd76
kekzdealer <kekzdealer@gmail.com>
2022-07-02 19:41:24 +0200

    wee

--- src/main/java/at/kurumi/HeartBeat.java
+++ src/main/java/at/kurumi/HeartBeat.java
@@ -1,26 +1,27 @@
 package at.kurumi;
 
-import at.kurumi.routines.StartupRoutine;
-import jakarta.enterprise.context.RequestScoped;
-import jakarta.inject.Inject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.core.Response;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 /**
  * HTTP Endpoint to check if payara is still running.
  */
-@RequestScoped
-@Path("heartbeat")
+@WebServlet(
+        name = "HeartBeat",
+        description = "Check if the Server is still running",
+        urlPatterns = {"/heartbeat"}
+)
 public class HeartBeat extends HttpServlet {
 
-    @Inject
-    private StartupRoutine fuck;
-
-    @GET
-    public Response healthCheck() {
-        fuck.toString();
-        return Response.ok().entity("I'm alive\n").build();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        var out = resp.getWriter();
+        out.println("<p>I'm alive</p>");
     }
 }
