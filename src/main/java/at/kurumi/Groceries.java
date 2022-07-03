ea399fe059f94f944302bec706b1fb192c9f57f2
kekzdealer <kekzdealer@gmail.com>
2022-07-03 23:19:55 +0200

    lil test

--- src/main/java/at/kurumi/Groceries.java
+++ src/main/java/at/kurumi/Groceries.java
@@ -2,13 +2,16 @@ package at.kurumi;
 
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
 
-@Path("groceries")
+@Path("/groceries")
 public class Groceries {
 
     @POST
-    @Path("add")
+    @Path("/add")
+    @Produces(MediaType.TEXT_PLAIN)
     public String add(@QueryParam("name") String item) {
         return "added: " + item;
     }
